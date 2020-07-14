e878e4fc2c84fe4767dbcfce36e35fad72afa728
Jason Beggs <jason@roasted.dev>
2020-07-14 10:15:48 -0400

    Initial

---
+++ src/Analytics.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Northeastern\Support;
+
+class Analytics
+{
+    /**
+     * Retrieve Northeastern Google Tag Manager script
+     *
+     * @return string
+     */
+    public static function googleTagManagerScript()
+    {
+        return "
+            <!-- Google Tag Manager -->
+            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WGQLLJ');</script>
+            <!-- End Google Tag Manager -->
+        ";
+    }
+
+    /**
+     * Retrieve Northeastern Google Tag Manager noscript
+     *
+     * @return string
+     */
+    public static function googleTagManagerNoScript()
+    {
+        return '
+            <!-- Google Tag Manager (noscript) -->
+            <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WGQLLJ"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+            <!-- End Google Tag Manager (noscript) -->
+        ';
+    }
+}
